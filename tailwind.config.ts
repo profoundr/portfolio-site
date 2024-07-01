55ecda24c57eec8a06c12408746350113481b2f0
Kedar Sawant <kedar.sawant66@gmail.com>
2024-07-02 04:18:17 +0530

    First commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,19 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize:{
+      'large': '2.75rem',
+      'small': '1.75rem',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      maxWidth: {
+        'screen-3xl': '1980px',
+      }
     },
   },
   plugins: [],
