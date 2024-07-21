cb192266f3c1438da905b9d65f74f46de3c76157
Kedar Sawant <kedar.sawant66@gmail.com>
2024-07-22 03:53:13 +0530

    chore: Add EmblaCarousel and EmblaCarouselDotButton components

---
+++ app/components/SecondColumn.tsx
@@ -0,0 +1,64 @@
+"use client";
+import useEmblaCarousel from "embla-carousel-react";
+import { WebsiteCard } from "../page";
+import { useKeenSlider } from "keen-slider/react"; // import from 'keen-slider/react.es' for to get an ES module
+import "keen-slider/keen-slider.min.css";
+import EmblaCarousel from "./Embla/EmblaCarousel";
+import { EmblaOptionsType } from "embla-carousel";
+
+export function SecondColumn() {
+  const [emblaRef] = useEmblaCarousel();
+  const OPTIONS: EmblaOptionsType = { dragFree: true, axis: 'y'  };
+  const SLIDE_COUNT = 7;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+
+  return (
+    <div className="w-1/2 flex items-center">
+      {/* <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+          <div className="embla__slide">
+            <WebsiteCard number={1} />
+          </div>
+          <div className="embla__slide">
+            <WebsiteCard number={2} />
+          </div>
+          <div className="embla__slide">
+            <WebsiteCard number={3} />
+          </div>
+          <div className="embla__slide">
+            <WebsiteCard number={4} />
+          </div>
+        </div>
+      </div> */}
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+
+      {/* <Slider /> */}
+    </div>
+  );
+}
+export function Slider() {
+  const [ref] = useKeenSlider<HTMLDivElement>();
+
+  return (
+    <div ref={ref} className="keen-slider w-full">
+      <div className="keen-slider__slide w-[500px] flex justify-center items-center h-[500px] bg-slate-500">
+        1
+      </div>
+      <div className="keen-slider__slide w-[500px] flex justify-center items-center h-[500px] bg-slate-500 ">
+        2
+      </div>
+      <div className="keen-slider__slide w-[500px] flex justify-center items-center h-[500px] bg-slate-500 ">
+        3
+      </div>
+      <div className="keen-slider__slide w-[500px] flex justify-center items-center h-[500px] bg-slate-500 ">
+        4
+      </div>
+      <div className="keen-slider__slide w-[500px] flex justify-center items-center h-[500px] bg-slate-500 ">
+        5
+      </div>
+      <div className="keen-slider__slide w-[500px] flex justify-center items-center h-[500px] bg-slate-500 ">
+        6
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,130 +1,143 @@
 import Image from "next/image";
+import { SecondColumn } from "./components/SecondColumn";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen p-24">
       <div className="flex w-full max-w-screen-xl">
-        <div className="w-1/2 flex flex-col justify-center">
-          <span className="text-large">Hey welcome to [name]</span>
-          <span className="text-large">
-            I am a freelance software developer
-          </span>
-          <span className="text-small">
-            My name is [], I am a web developer who makes web apps leveraging
-            AI.
-          </span>
-          <div className="relative mt-4">
-            <span className="absolute bg-white  top-[-8px] left-4 text-[12px] px-1">
-              Techstack
-            </span>
-            <div className="border-[1.5px] border-[#2f2f2f] rounded-lg grid grid-cols-5 p-4 justify-items-center items-end">
-              <div className="w-fit flex flex-col items-center justify-between">
-                <Image
-                  src="/SVGs/nextjs.svg"
-                  width={60}
-                  height={50}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Next.JS</span>
-              </div>
-              <div className="w-fit flex flex-col items-center justify-between">
-                <Image
-                  src="/SVGs/Remix.svg"
-                  width={50}
-                  height={50}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Remix</span>
-              </div>
-              <div className="w-fit flex flex-col items-center gap-2">
-                <Image
-                  src="/SVGs/TailwindCSS.svg"
-                  width={70}
-                  height={40}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Tailwind CSS</span>
-              </div>{" "}
-              <div className="w-fit flex flex-col items-center justify-between">
-                <Image
-                  src="/SVGs/shopify.svg"
-                  width={50}
-                  height={50}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Shopify</span>
-              </div>{" "}
-              <div className="w-fit flex flex-col items-center justify-between">
-                <Image
-                  src="/SVGs/DirectusCMS.svg"
-                  width={70}
-                  height={50}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Directus CMS</span>
-              </div>
-            </div>
-          </div>
-          <div className="relative mt-4">
-            <span className="absolute bg-white  top-[-8px] left-4 text-[12px] px-1">
-              AI Techstack
-            </span>
-            <div className="border-[1.5px] border-[#2f2f2f] rounded-lg grid grid-cols-4 p-4 justify-items-center items-end">
-              <div className="w-fit flex flex-col items-center justify-between">
-                <Image
-                  src="/SVGs/Copilot.svg"
-                  width={60}
-                  height={50}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Github CoPilot</span>
-              </div>
-              <div className="w-fit flex flex-col items-center gap-2">
-                <Image
-                  src="/SVGs/OpenAI.svg"
-                  width={60}
-                  height={40}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Chat GPT</span>
-              </div>{" "}
-              <div className="w-fit flex flex-col items-center justify-between">
-                <Image
-                  src="/SVGs/shopify.svg"
-                  width={50}
-                  height={50}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Shopify</span>
-              </div>{" "}
-              <div className="w-fit flex flex-col items-center justify-between">
-                <Image
-                  src="/SVGs/DirectusCMS.svg"
-                  width={70}
-                  height={50}
-                  alt="Next.js"
-                  className="mb-2"
-                />
-                <span className="text-[12px] mx-auto">Directus CMS</span>
-              </div>
-            </div>
-          </div>
-          <button className="mt-4 bg-[#2f2f2f] text-white py-4 px-4 rounded-lg">
-            Let&apos;s connect
-          </button>
-        </div>
-        <div className="w-1/2 flex items-center">
-          <span>yo</span>
-        </div>
+        <FirstColumn />
+
+        <SecondColumn />
       </div>
     </main>
+  );
+}
+
+function FirstColumn() {
+  return (
+    <div className="w-1/2 flex flex-col justify-center">
+      <span className="text-large">Hey welcome to [name]</span>
+      <span className="text-large">I am a freelance software developer</span>
+      <span className="text-small">
+        My name is [], I am a web developer who makes web apps leveraging AI.
+      </span>
+      <div className="relative mt-4">
+        <span className="absolute bg-white  top-[-8px] left-4 text-[12px] px-1">
+          Techstack
+        </span>
+        <div className="border-[1.5px] border-[#2f2f2f] rounded-lg grid grid-cols-5 p-4 justify-items-center items-end">
+          <div className="w-fit flex flex-col items-center justify-between">
+            <Image
+              src="/SVGs/nextjs.svg"
+              width={60}
+              height={50}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Next.JS</span>
+          </div>
+          <div className="w-fit flex flex-col items-center justify-between">
+            <Image
+              src="/SVGs/Remix.svg"
+              width={50}
+              height={50}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Remix</span>
+          </div>
+          <div className="w-fit flex flex-col items-center gap-2">
+            <Image
+              src="/SVGs/TailwindCSS.svg"
+              width={70}
+              height={40}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Tailwind CSS</span>
+          </div>{" "}
+          <div className="w-fit flex flex-col items-center justify-between">
+            <Image
+              src="/SVGs/shopify.svg"
+              width={50}
+              height={50}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Shopify</span>
+          </div>{" "}
+          <div className="w-fit flex flex-col items-center justify-between">
+            <Image
+              src="/SVGs/DirectusCMS.svg"
+              width={70}
+              height={50}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Directus CMS</span>
+          </div>
+        </div>
+      </div>
+      <div className="relative mt-4">
+        <span className="absolute bg-white  top-[-8px] left-4 text-[12px] px-1">
+          AI Techstack
+        </span>
+        <div className="border-[1.5px] border-[#2f2f2f] rounded-lg grid grid-cols-4 p-4 justify-items-center items-end">
+          <div className="w-fit flex flex-col items-center justify-between">
+            <Image
+              src="/SVGs/Copilot.svg"
+              width={60}
+              height={50}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Github CoPilot</span>
+          </div>
+          <div className="w-fit flex flex-col items-center gap-2">
+            <Image
+              src="/SVGs/OpenAI.svg"
+              width={60}
+              height={40}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Chat GPT</span>
+          </div>{" "}
+          <div className="w-fit flex flex-col items-center justify-between">
+            <Image
+              src="/SVGs/shopify.svg"
+              width={50}
+              height={50}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Shopify</span>
+          </div>{" "}
+          <div className="w-fit flex flex-col items-center justify-between">
+            <Image
+              src="/SVGs/DirectusCMS.svg"
+              width={70}
+              height={50}
+              alt="Next.js"
+              className="mb-2"
+            />
+            <span className="text-[12px] mx-auto">Directus CMS</span>
+          </div>
+        </div>
+      </div>
+      <button className="mt-4 bg-[#2f2f2f] text-white py-4 px-4 rounded-lg">
+        Let&apos;s connect
+      </button>
+    </div>
+  );
+}
+
+
+
+export function WebsiteCard({ number }: { number: number }) {
+  return (
+    <div className="w-1/2 flex items-center">
+      <span>{number}</span>
+    </div>
   );
 }
